d9c0b5a02f0ea9970e7558263db0a0f2ed5cab8c
Henry Thasler <git@thasler.org>
2014-07-10 19:29:49 +0200

    Initial repo setup

--- map.js
+++ map.js
@@ -8,29 +8,6 @@ String.prototype.format = function() {
   });
 };
 
-
-// http://www.javascriptkit.com/jsref/regexp.shtml
-// based on http://stackoverflow.com/a/3890175
-function makeLink(inputText) {
-    var replacedText, replacePattern1, replacePattern2, replacePattern3;
-
-    //URLs starting with http://, https://, or ftp://
-    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
-
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(?:^|[^\/])(www\.[\S]+(?:\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '<a href="http://$1" target="_blank">$1</a>');
-
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /([\w-.]+@[\w-]+\.[a-z]{2,6})/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-
-    return replacedText;
-}
-
-
-
 function getTrackProps(points)
 {
 var 	props = {},
@@ -133,6 +110,21 @@ $(document).ready(function()
 {
 	var CurrentLayer;
 	var xhr; // holds the last ajax request
+
+	var map = L.map('map', {
+		center: [48.24, 11.03], 
+		zoom: 12
+		});
+
+	var lat = $.getQuery('lat'),
+		lng = $.getQuery('lng'),
+		zoom = $.getQuery('zoom');
+
+	if(lat || lng || zoom)
+		map.setView(new L.LatLng( (lat?lat:48.24), (lng?lng:11.03) ), (zoom?zoom:12));
+	else 
+		map.locate({setView: true, maxZoom: 16});
+	
 	
 	var Queries = {
 		restaurant:'node["amenity"~"restaurant|fast_food"]({0});out body 200;node["tourism"="alpine_hut"]({0});out body 200;',
@@ -148,6 +140,14 @@ $(document).ready(function()
 		supermarket_way:'(way["shop"="supermarket"]({0});node(w););out body;node["shop"="supermarket"]({0});out body 100;'
 		};
 		
+	// L.control.scale({imperial: false}).addTo(map);
+		
+	L.tileLayer('tiles/{z}/{x}/{y}.png', {
+		minZoom: 1,
+		maxZoom: 14,
+		attribution: 'v'+L.version+',  Map data &copy; 2012 OpenStreetMap, Renderstyle &copy; 2012 Henry Thasler'
+		}).addTo(map);
+
 	var AIcon = L.icon({
 		iconUrl: 'img/a-marker.png',
 		iconAnchor: [12, 40],
@@ -195,11 +195,60 @@ $(document).ready(function()
 		iconSize: [32, 37],
 		iconAnchor: [16, 37],
 		popupAnchor: [0, -28]
-	});
+	});	
+
+	function LoadOSM(layer)
+	{
+		if(!layer || !layer.options.query) return;
+		var lat1 = map.getBounds().getSouthWest().lat;
+		var lat2 = map.getBounds().getNorthEast().lat;
+		var lng1 = map.getBounds().getSouthWest().lng;
+		var lng2 = map.getBounds().getNorthEast().lng;
+		var bbox = ''+Math.min(lat1,lat2)+','+Math.min(lng1,lng2)+','+Math.max(lat1,lat2)+','+Math.max(lng1,lng2);
+		if(xhr) xhr.abort();	// abort last request if not finished
+		xhr = $.ajax({
+			 // url: "proxy.php?bbox=[bbox="+map.getBounds().toBBoxString()+"]&q=[amenity=restaurant|fast_food][tourism=alpine_hut]",
+			 url: 'proxy.php?q='+ (map.getZoom()<13?layer.options.query.low:layer.options.query.high).format(bbox),
+			 success:function(data){
+				 // do stuff with json (in this case an array)
+				 // alert(data);
+				var geojsonFeature = osm2geo(data);
+				// console.log(geojsonFeature);
+				layer.clearLayers();
+				layer.addData(geojsonFeature);
+			 },
+			error: function(jqXHR, exception) {
+				if (jqXHR.status === 0) {
+					// alert('Not connect.\n Verify Network.');
+				} else if (jqXHR.status == 404) {
+					alert('Requested page not found. [404]');
+				} else if (jqXHR.status == 500) {
+					alert('Internal Server Error [500].');
+				} else if (exception === 'parsererror') {
+					alert('Requested JSON parse failed.');
+				} else if (exception === 'timeout') {
+					alert('Time out error.');
+				} else if (exception === 'abort') {
+					alert('Ajax request aborted.');
+				} else {
+					alert('Uncaught Error.\n' + jqXHR.responseText);
+				}
+			 },
+		});
+	}
 	
-	var lat  = $.getQuery('lat'),
-	    lng  = $.getQuery('lng'),
-	    zoom = $.getQuery('zoom');
+	function onEachFeature(feature, layer) {
+		var popupContent = "";
+		for(ele in feature.properties)
+			{
+			popupContent += ele+"=<b>"+feature.properties[ele]+"</b></br>";
+			}
+		// console.log(layer);	
+		layer.bindPopup(popupContent);
+		// layer.geometryToLayer(
+		// var SomeMarker = L.marker(layer.getBounds().getCenter(), {icon: RestaurantIcon}).bindPopup(popupContent);
+		// layer.addLayer(new L.Marker(new L.LatLng(10.9623409,48.303125)));
+	}
 
 	var EmptyLayer = L.geoJson(0, {});
 	
@@ -242,61 +291,7 @@ $(document).ready(function()
 			return L.marker(latlng, {icon: ShopIcon, riseOnHover:true});
 		}
 	});
-    
-
-// sqlite-based	php tileserver
-	var funcLayer = new L.tileLayer('tileserver.php?z={z}&x={x}&y={y}', {
-		minZoom: 1,
-		maxZoom: 18,
-		maxNativeZoom: 15,
-		continuousWorld: false,
-		noWrap: true,
-		attribution: 'v'+L.version+',  Map data &copy; OpenStreetMap, Renderstyle &copy; Henry Thasler'
-		});
-
-
-/*
-// file-based tiles
-	var funcLayer = new L.tileLayer('tiles/{z}/{x}/{y}.png', {
-		minZoom: 1,
-		maxZoom: 18,
-		maxNativeZoom: 15,
-		continuousWorld: false,
-		noWrap: true,
-		attribution: 'v'+L.version+',  Map data &copy; OpenStreetMap, Renderstyle &copy; Henry Thasler'
-		});
-*/
-
-/*
-	  var funcLayer = new L.TileLayer.Functional(function (view) {
-	      var url = 'tiles/{z}/{x}/{y}.png'
-	        .replace('{z}', view.zoom)
-	        .replace('{x}', view.tile.column)
-	        .replace('{y}', view.tile.row);
 	
-		url = 'tileserver.php';
-	      return url;
-	    }, {
-		minZoom: 1,
-		maxZoom: 18,
-		maxNativeZoom: 15,
-		continuousWorld: false,
-		noWrap: true,
-		attribution: 'v'+L.version+',  Map data &copy; OpenStreetMap, Renderstyle &copy; Henry Thasler'
-	    });	
-*/		
-	
-	var map = L.map('map', {
-		center: [48.24, 11.03], 
-		zoom: 12,
-		layers: [funcLayer]
-		});
-
-	if(lat || lng || zoom)
-		map.setView(new L.LatLng( (lat?lat:48.24), (lng?lng:11.03) ), (zoom?zoom:12));
-	else 
-		map.locate({setView: true, maxZoom: 16});
-
 
 // Add layer control
 	var overlayMaps = {
@@ -308,71 +303,10 @@ $(document).ready(function()
 		"Supermarket": ShopMarker
 	};	
 	L.control.layers(overlayMaps, 0, {collapsed: false}).addTo(map);
-
 	
 // Add Route polyline	
-	var Route = L.polyline([], {color: 'red', opacity: 0.5, clickable:false, editable:true}).addTo(map);
+	var polygon = L.polyline([], {color: 'red', opacity: 0.5, clickable:false, editable:true}).addTo(map);
 
-
-// Add Track Profile Window	
-	//all used options are the default values
-	var Profile = L.control.elevation({
-	    position: "bottomleft",
-	    theme: "steelblue-theme", //default: lime-theme
-	    width: 600,
-	    height: 125,
-	    margins: {
-		top: 10,
-		right: 20,
-		bottom: 30,
-		left: 50
-	    },
-	    useHeightIndicator: true, //if false a marker is drawn at map position
-	    interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-	    hoverNumber: {
-		decimalsX: 3, //decimals on distance (always in km)
-		decimalsY: 0, //deciamls on height (always in m)
-		formatter: undefined //custom formatter function may be injected
-	    },
-	    xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-	    yTicks: undefined, //number of ticks on y axis, calculated by default according to height
-	    collapsed: false    //collapsed mode, show chart on click or mouseover
-	});
-	Profile.addTo(map);
-	Profile._collapse();
-	
-	var GPXData = new L.GPX(null, {
-		async: true,
-		marker_options: {
-		      startIconUrl: 'img/a-marker.png',
-		      endIconUrl: 'img/b-marker.png',
-		      shadowUrl: 'img/marker-shadow.png',
-		}
-	});
-				
-	GPXData.on('loaded', function(e) {
-		map.fitBounds(e.target.getBounds());
-	});
-				
-	GPXData.on("addline",function(e){
-		Profile.clear();  
-		Profile._expand();
-		Profile.addData(e.line);
-	});
-	
-	GPXData.on("addroute",function(e){
-		Route.editing.disable();
-		var arr = [];
-		for (ele in e.c) arr.push(e.c[ele]);
-		Route.setLatLngs(arr);
-		Route.editing.enable();
-		map.fitBounds(Route.getBounds());
-		info.update(getTrackProps(arr));
-	});
-	
-	GPXData.addTo(map);
-
-	
 // Add Track polyline	
 	var track = L.polyline([], {color: 'magenta', opacity: 0.5}).addTo(map);
 
@@ -435,7 +369,7 @@ $(document).ready(function()
 
 	};
 	info.addTo(map);
-
+	
 // Add permalink and position info	
 	var permalink = L.control({position: 'topright'});
 	permalink.onAdd = function (map) {
@@ -473,10 +407,10 @@ $(document).ready(function()
 		new_route.title = 'create new route';
 		L.DomEvent.addListener(new_route, 'click', function(e){
 			L.DomEvent.stopPropagation(e);
-			Route.editing.disable();
-			Route.spliceLatLngs(0);
-			Route.editing.enable();
-			info.update(getTrackProps(Route.getLatLngs()));
+			polygon.editing.disable();
+			polygon.spliceLatLngs(0);
+			polygon.editing.enable();
+			info.update(getTrackProps(polygon.getLatLngs()));
 			}, this);
 
 		// open button
@@ -492,53 +426,13 @@ $(document).ready(function()
 		return this._div;
 	};
 	tools.addTo(map);
+
 	
 // Setup the dnd listeners.
 	var dropZone = document.getElementById('map');
 	dropZone.addEventListener('dragover', handleDragOver, false);
 	dropZone.addEventListener('drop', handleFileSelect, false);	
-
-	Route.on('edit', function(e)
-	{
-	  info.update(getTrackProps(Route.getLatLngs()));
-	});
 	
-	map.on('drag' , function()
-	{
-	  permalink.update({lng:map.getCenter().lng, lat:map.getCenter().lat, zoom:map.getZoom()});
-	});
-
-	map.on('dragend' , function()
-	{
-	  LoadOSM(CurrentLayer);
-	});
-
-	map.on('zoomend' , function()
-	{
-	  permalink.update({lng:map.getCenter().lng, lat:map.getCenter().lat, zoom:map.getZoom()});
-	  LoadOSM(CurrentLayer);
-	});
-	
-	map.on('baselayerchange', function(e) 
-	{
-	  CurrentLayer = e.layer;
-	  LoadOSM(CurrentLayer);
-	});
-	
-	map.on('click', function(e) 
-	{
-	  Route.editing.disable();
-	  Route.addLatLng(e.latlng);
-	  Route.editing.enable();
-	  info.update(getTrackProps(Route.getLatLngs()));
-	  // map.panTo(e.latlng);
-	});
-
-	map.on('locationfound', onLocationFound);
-	map.on('locationerror', onLocationError);
-	
-	
-// geolocation handler	
 	function onLocationFound(e) {
 		var radius = e.accuracy / 2;
 
@@ -550,69 +444,50 @@ $(document).ready(function()
 	function onLocationError(e) {
 		alert(e.message);
 	}
-		
-	function LoadOSM(layer)
+	
+	polygon.on('edit', function(e)
 	{
-		if(!layer || !layer.options.query) return;
-		var lat1 = map.getBounds().getSouthWest().lat;
-		var lat2 = map.getBounds().getNorthEast().lat;
-		var lng1 = map.getBounds().getSouthWest().lng;
-		var lng2 = map.getBounds().getNorthEast().lng;
-		var bbox = ''+Math.min(lat1,lat2)+','+Math.min(lng1,lng2)+','+Math.max(lat1,lat2)+','+Math.max(lng1,lng2);
-		if(xhr) xhr.abort();	// abort last request if not finished
-		xhr = $.ajax({
-			 // url: "proxy.php?bbox=[bbox="+map.getBounds().toBBoxString()+"]&q=[amenity=restaurant|fast_food][tourism=alpine_hut]",
-			 url: 'proxy.php?q='+ (map.getZoom()<13?layer.options.query.low:layer.options.query.high).format(bbox),
-//			 url: 'http://overpass-api.de/api/interpreter?data='+ (map.getZoom()<13?layer.options.query.low:layer.options.query.high).format(bbox),
-			 success:function(data){
-				 // do stuff with json (in this case an array)
-				 // alert(data);
-				var geojsonFeature = osm2geo(data);
-//				geojsonFeature.on("featureparse", function(e) {alert(e);});
-				layer.clearLayers();
-				layer.addData(geojsonFeature);
-			 },
-			error: function(jqXHR, exception) {
-				if (jqXHR.status === 0) {
-					// alert('Not connect.\n Verify Network.');
-				} else if (jqXHR.status == 404) {
-					alert('Requested page not found. [404]');
-				} else if (jqXHR.status == 500) {
-					alert('Internal Server Error [500].');
-				} else if (exception === 'parsererror') {
-					alert('Requested JSON parse failed.');
-				} else if (exception === 'timeout') {
-					alert('Time out error.');
-				} else if (exception === 'abort') {
-					alert('Ajax request aborted.');
-				} else {
-					alert('Uncaught Error.\n' + jqXHR.responseText);
-				}
-			 },
-		});
-	}
+	info.update(getTrackProps(polygon.getLatLngs()));
+	});
 	
-	function onEachFeature(feature, layer) {
-		var popupContent = "";
-		for(ele in feature.properties)
-			{
-			popupContent += ele+"=<b>"+makeLink(feature.properties[ele])+"</b></br>";
-			}
-		// console.log(layer);	
-		layer.bindPopup(popupContent);
-		
-		// layer.geometryToLayer(
-//		L.marker(layer.getBounds().getCenter(), {icon: RestaurantIcon}).bindPopup(popupContent).addTo(map);
-		//layer.addLayer(new L.Marker(new L.LatLng(10.9623409,48.303125)));
-	}
+	map.on('drag' , function()
+	{
+	permalink.update({lng:map.getCenter().lng, lat:map.getCenter().lat, zoom:map.getZoom()});
+	});
+
+	map.on('dragend' , function()
+	{
+	LoadOSM(CurrentLayer);
+	});
+
+	map.on('zoomend' , function()
+	{
+	LoadOSM(CurrentLayer);
+	});
 	
+	map.on('baselayerchange', function(e) {
+	 CurrentLayer = e.layer;
+     LoadOSM(CurrentLayer);
+	});
+	
+	map.on('click', function(e) 
+	{
+	polygon.editing.disable();
+	polygon.addLatLng(e.latlng);
+	polygon.editing.enable();
+	info.update(getTrackProps(polygon.getLatLngs()));
+	// map.panTo(e.latlng);
+	});
+
+	map.on('locationfound', onLocationFound);
+	map.on('locationerror', onLocationError);
 	
 	function DownloadRoute()
 	{
 		var gpx="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" creator=\"MyCycleMap by Henry Thasler www.thasler.org/map\" version=\"1.1\">\n<rte>\n{0}</rte>\n</gpx>";
 		var rtept="\t<rtept lon=\"{0}\" lat=\"{1}\">\n\t\t<name>{2}</name>\n\t</rtept>\n";
 		var rte="";
-		var pos = Route.getLatLngs();
+		var pos = polygon.getLatLngs();
 		if(pos.length<=0) alert("Route empty.");
 		else
 			{
@@ -670,15 +545,8 @@ $(document).ready(function()
 
 		if (window.DOMParser) 
 			{
-			try{  
-			    parser = new DOMParser();
-			    xmlDoc = parser.parseFromString(text, "text/xml");
-			   }
-			catch(e)
-			  {
-			  alert ("XML parsing error.");
-			  return false;			    
-			  }
+			parser = new DOMParser();
+			xmlDoc = parser.parseFromString(text, "text/xml");
 			}
 		else // Internet Explorer
 			{
@@ -824,9 +692,7 @@ $(document).ready(function()
 			EndMarker.setLatLng(points[points.length-1]);
 			map.fitBounds(track.getBounds());
 			// track.openPopup();
-			
-			if(document.getElementById("myCanvas") == null) 
-			    profile.addTo(map);
+			profile.addTo(map);
 			
 			// draw height profile
 			var min_ele=ele[0], max_ele;
@@ -844,7 +710,6 @@ $(document).ready(function()
 			
 			var c=document.getElementById("myCanvas");
 			var ctx=c.getContext("2d");
-			
 			var center_ele = min_ele+(max_ele-min_ele)/2;
 			var canvas_height=150, border=0, left=0;
 
@@ -865,7 +730,7 @@ $(document).ready(function()
 			ctx.strokeStyle="#0000AA";
 			
 			ctx.moveTo(left, canvas_height-border-(ele[0]-min_ele)/(max_ele-min_ele)*(canvas_height-2*border));
-			var step=Math.max(parseInt(points.length/canvas_width),1);
+			var step=parseInt(points.length/canvas_width);
 			for(x=step;x<points.length;x+=step)
 				{
 				ctx.lineTo(left+distance[x]/distance[points.length-step]*canvas_width, canvas_height-border-(ele[x]-min_ele)/(max_ele-min_ele)*(canvas_height-2*border));
@@ -880,14 +745,14 @@ $(document).ready(function()
 	  
 		if(TrackData.routes.length)
 			{
-			Route.editing.disable();
-			Route.spliceLatLngs(0);
+			polygon.editing.disable();
+			polygon.spliceLatLngs(0);
 			var t = TrackData.routes[0].points.length;
 			for(var x=0;x<t;x++)
-				Route.addLatLng(TrackData.routes[0].points[x].latLng);
-			map.fitBounds(Route.getBounds());
-			Route.editing.enable();
-			info.update(getTrackProps(Route.getLatLngs()));
+				polygon.addLatLng(TrackData.routes[0].points[x].latLng);
+			map.fitBounds(polygon.getBounds());
+			polygon.editing.enable();
+			info.update(getTrackProps(polygon.getLatLngs()));
 			}
 
 	}
@@ -910,23 +775,7 @@ $(document).ready(function()
 			  reader.onload = (function(theFile) {
 				return function(e) 
 				{
-				GPXData.load(e.target.result);  // this is a special modification
-				/*				  
-				  new L.GPX(e.target.result, {
-				    async: true,
-				    marker_options: {
-				      startIconUrl: 'img/a-marker.png',
-				      endIconUrl: 'img/b-marker.png',
-				      shadowUrl: 'img/marker-shadow.png',
-				      iconAnchor: [1, 40]
-
-				    }				    
-				    }).on('loaded', function(e) {
-				    map.fitBounds(e.target.getBounds());
-				  }).addTo(map);
-				*/
-			  
-				//loadGPXData(textToXml(e.target.result));
+				loadGPXData(textToXml(e.target.result));
 				};
 			  })(f);
 
@@ -940,5 +789,4 @@ $(document).ready(function()
 		evt.preventDefault();
 		evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 	}	
-	
 });
